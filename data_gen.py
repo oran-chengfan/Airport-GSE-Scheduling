b28a0521938ca92f1853b265d26d47bd6d4667ce
fc-001 <2251140@tongji.edu.cn>
2026-03-26 17:22:44 +0800

    change the generation logic

--- data_gen.py
+++ data_gen.py
@@ -3,17 +3,18 @@ import numpy as np
 import os
 import argparse
 
-def generate_dynamic_wind_tunnel(num_days, num_flights, seed, num_peaks=2):
+def generate_dynamic_wind_tunnel(num_days, num_flights, seed,target_K,num_peaks=2):
     records = []
     np.random.seed(seed)
     
+    critical_interval = 60/target_K
     for day in range(num_days):
         peak_centers = np.random.uniform(0.25, 0.75, num_peaks) * num_flights
         intervals = []
         for i in range(num_flights):
             min_dist = min([abs(i - pc) for pc in peak_centers])
             density_factor = np.exp(- (min_dist ** 2) / (0.05 * num_flights ** 2))
-            iv = int(20 - 10 * density_factor + np.random.normal(0, 2))
+            iv = int(25 - (25 - critical_interval) * density_factor + np.random.normal(0, 1))
             intervals.append(max(5, iv))
             
         base_sta = [720]
@@ -56,6 +57,7 @@ if __name__ == "__main__":
     parser.add_argument("-D", "--days", type=int, default=30, help="训练集的天数")
     parser.add_argument("-F", "--flights", type=int, default=15, help="每天的航班数")
     parser.add_argument("-S", "--seed", type=int, default=42, help="随机种子")
+    parser.add_argument("-K", "--target_K", type=int, default=10, help="车数量")
     args = parser.parse_args()
 
     os.makedirs("toy_data", exist_ok=True)
@@ -63,9 +65,9 @@ if __name__ == "__main__":
     val_days = max(1, int(args.days * 0.2))
     test_days = max(1, int(args.days * 0.3))
 
-    df_train = generate_dynamic_wind_tunnel(args.days, args.flights, args.seed)
-    df_val = generate_dynamic_wind_tunnel(val_days, args.flights, args.seed + 1)
-    df_test = generate_dynamic_wind_tunnel(test_days, args.flights, args.seed + 2)
+    df_train = generate_dynamic_wind_tunnel(args.days, args.flights,args.target_K, args.seed)
+    df_val = generate_dynamic_wind_tunnel(val_days, args.flights,args.target_K, args.seed + 1)
+    df_test = generate_dynamic_wind_tunnel(test_days, args.flights,args.target_K, args.seed + 2)
 
     base_name = f"toy_data/D{args.days}-F{args.flights}-S{args.seed}"
     df_train.to_csv(f"{base_name}-Train.csv", index=False)
